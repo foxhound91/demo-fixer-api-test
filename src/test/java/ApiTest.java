41cc58639c882b84ce124666dcf4b020fe6683ca
franco <franco.pascual@concirrus.com>
2020-01-24 15:58:47 +0000

    more test scenarios added

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -1,4 +1,7 @@
+import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.specification.RequestSpecification;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -23,20 +26,42 @@ public class ApiTest {
                 .param("access_key", apiAccessKey);
     }
 
-    @Test
+    @Test // Take the same scenario as above
+    public void testGetPricesForFiveCurrencies_statusCodeIsOk() {
+        requestSpecification.when().get(url + "/latest&symbols=USD,AUD,CAD,PLN,MXN")
+                .then().statusCode(200);
+    }
+
+    @Test // Take the same scenario as above
+    public void testGetPricesForFiveCurrencies_bodySuccessIsTrue() {
+        requestSpecification.when().get(url + "/latest&symbols=USD,AUD,CAD,PLN,MXN")
+                .then().body("success", equalTo(true));
+    }
+
+    @Test // Take the same scenario as above
+    public void testGetPricesForFiveCurrencies_mediaTypeIsJson() {
+        requestSpecification.when().get(url + "/latest&symbols=USD,AUD,CAD,PLN,MXN")
+                .then().contentType(ContentType.JSON);
+    }
+
+    @Test // Take the same scenario as above
+    public void testGetPricesForFiveCurrencies_responseTimeLessThanTwoSec() {
+        requestSpecification.when().get(url + "/latest&symbols=USD,AUD,CAD,PLN,MXN")
+                .then().time(Matchers.lessThan(2000L));
+    }
+
+    @Test // Take the same scenario as above
+    public void testGetPricesForFiveCurrencies_schemaValidation() {
+        requestSpecification.when().get(url + "/latest&symbols=USD,AUD,CAD,PLN,MXN")
+                .then().assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas/getCurrenciesExchangeRate.json"));
+    }
+
+    @Test // Check the site is responsive
     public void testSiteIsResponsive() {
         requestSpecification.when().get(url).then().statusCode(200);
     }
 
-    @Test
-    public void testGetPricesForFiveCurrencies() {
-        requestSpecification.when().get(url + "/latest&symbols=USD,AUD,CAD,PLN,MXN")
-                .then()
-                    .statusCode(200)
-                    .body("success", equalTo(true));
-    }
-
-    @Test
+    @Test // Run the query to return all Currencies
     public void testGetAllCurrencies() {
         requestSpecification.when().get(url + "/latest")
                 .then()
@@ -44,7 +69,7 @@ public class ApiTest {
                     .body("success", equalTo(true));
     }
 
-    @Test
+    @Test // Parse the JSON for 10 random Currencies and store the key and value into a CSV file
     public void testTenRandomCurrenciesToCSV() throws Exception {
         CsvHelper csvHelper = new CsvHelper();
         Map<String, Float> currenciesMap;
