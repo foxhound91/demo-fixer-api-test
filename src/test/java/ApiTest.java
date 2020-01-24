a07c873b626e7522db77cb3cbfb0e91ea45e4e06
franco <franco.pascual@concirrus.com>
2020-01-24 14:22:31 +0000

    unit tests implemented

---
+++ src/test/java/ApiTest.java
@@ -0,0 +1,54 @@
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.Random;
+
+import static io.restassured.RestAssured.given;
+
+public class ApiTest {
+
+    private String url = "http://data.fixer.io/api";
+    private RequestSpecification requestSpecification;
+
+    @Before
+    public void setUp() {
+        String apiAccessKey = "9b00f2faf87060519d0b0725a6f4af16";
+        requestSpecification = given()
+                .relaxedHTTPSValidation()
+                .log().uri()
+                .urlEncodingEnabled(false)
+                .param("access_key", apiAccessKey);
+    }
+
+    @Test
+    public void testSiteIsResponsive() {
+        requestSpecification.when().get(url).then().statusCode(200);
+    }
+
+    @Test
+    public void testGetPricesForFiveCurrencies() {
+        requestSpecification.when().get(url + "/latest&symbols=USD,AUD,CAD,PLN,MXN").then().statusCode(200);
+    }
+
+    @Test
+    public void testGetAllCurrencies() {
+        requestSpecification.when().get(url + "/latest").then().statusCode(200).log().ifError();
+    }
+
+    @Test
+    public void testTenRandomCurrenciesToCSV() throws Exception {
+        CsvHelper csvHelper = new CsvHelper();
+        Map<String, Float> currenciesMap;
+        currenciesMap = requestSpecification.when().get(url + "/latest").then().statusCode(200).extract().path("rates");
+
+        Random rand = new Random();
+        for (int x = 0; x < 10; x++) {
+            Object randomCurrency = currenciesMap.keySet().toArray()[rand.nextInt(currenciesMap.size())];
+            csvHelper.writeToCSV(randomCurrency.toString(), currenciesMap.get(randomCurrency));
+        }
+
+    }
+
+}
